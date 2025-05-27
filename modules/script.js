7757f721fbc20fc3d3d8c4741d654ec0254bf3b5
edupoly <info.edupoly@gmail.com>
2025-05-27 13:17:29 +0530

    uptojsx

---
+++ modules/script.js
@@ -0,0 +1,4 @@
+import  mul,{add,sub} from './utils.js'
+console.log(add(10,30));
+console.log(sub(44,5));
+console.log(mul(99,4));
---
+++ modules/utils.js
@@ -0,0 +1,11 @@
+export function add(a,b,c=0){
+    return a+b+c
+}
+
+export function sub(a,b){
+    return a-b;
+}
+
+export default function mul(a,b,c=1){
+    return a*b*c
+}
